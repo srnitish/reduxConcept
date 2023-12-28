128aad81c4e420dab801e4c418687fbadd0d56c5
Nitish Srivastava <sr.nitish06@gmail.com>
2023-12-28 19:59:29 +0530

    Updates

--- src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import allReducers from "../features/trySlice";
-
-export const store = configureStore({
-    reducer: {
-        //Here show is the key, from which we can access the data/value from the store i.e const data = useSelector((state) => state.show.value);
-        show: allReducers
-    },
-})
